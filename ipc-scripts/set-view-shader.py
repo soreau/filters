62da731b984435b338667c2d1b6e64860ac00e41
Scott Moreau <oreaus@gmail.com>
2024-03-17 02:50:33 -0600

    Use abspath to send full shader paths to fitlers plugin

--- ipc-scripts/set-view-shader.py
+++ ipc-scripts/set-view-shader.py
@@ -4,8 +4,12 @@ import os
 import sys
 from wayfire_socket import *
 
+if len(sys.argv) < 3:
+    print("Required arguments: <View ID> </path/to/shader>")
+    exit(-1)
+
 addr = os.getenv('WAYFIRE_SOCKET')
 
 commands_sock = WayfireSocket(addr)
 
-commands_sock.set_view_shader(int(sys.argv[1]), str(sys.argv[2]))
+commands_sock.set_view_shader(int(sys.argv[1]), os.path.abspath(str(sys.argv[2])))
