e6e1909861feddfc1758c45feabd916c52a63c80
Scott Moreau <oreaus@gmail.com>
2024-08-12 21:42:22 -0600

    Make set-inactive-views python script more robust

--- ipc-scripts/set-inactive-views.py
+++ ipc-scripts/set-inactive-views.py
@@ -22,22 +22,25 @@ def unset_view_shaders():
         wpe.filters_unset_view_shader(view["id"])
 
 active_view_id = sock.get_focused_view()["id"]
+active_view_app_id = sock.get_focused_view()["app-id"]
 for view in sock.list_views():
     view_id = view["id"]
-    app_id = view["app-id"]
     if view_id != active_view_id and view["app-id"] != "panel":
         wpe.filters_set_view_shader(view_id, os.path.abspath(str(sys.argv[1])))
 last_active_view_id = active_view_id
+last_active_view_app_id = active_view_app_id
 
 while True:
     try:
         msg = sock.read_next_event()
         if "event" in msg:
             active_view_id = sock.get_focused_view()["id"]
-            if last_active_view_id != active_view_id:
+            active_view_app_id = sock.get_focused_view()["app-id"]
+            if last_active_view_id != active_view_id and last_active_view_app_id != "panel":
                 wpe.filters_set_view_shader(last_active_view_id, os.path.abspath(str(sys.argv[1])))
                 wpe.filters_unset_view_shader(active_view_id)
             last_active_view_id = active_view_id
+            last_active_view_app_id = active_view_app_id
     except KeyboardInterrupt:
         unset_view_shaders()
         exit(0)
